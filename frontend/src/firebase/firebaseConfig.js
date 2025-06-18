019975f3dcb3156809c072aa024695e168f16b3e
Jagdish Padhi <jagdishpadhi9@gmail.com>
2025-06-18 16:39:27 +0530

    security fixes

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB61yehJ-iEMaLnan9mRU7WsFf-w5GCJKI",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "smartsahayak1.firebaseapp.com",
   projectId: "smartsahayak1",
   storageBucket: "smartsahayak1.firebasestorage.app",
@@ -12,5 +14,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
